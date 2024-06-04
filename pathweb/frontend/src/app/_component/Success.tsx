4a61de37de0cde9a1ca693aae038fafdeab57767
ChoiHongSeok <129854575+ChatHongPT@users.noreply.github.com>
2024-06-04 13:26:26 +0900

    Update Success.tsx

--- pathweb/frontend/src/app/_component/Success.tsx
+++ pathweb/frontend/src/app/_component/Success.tsx
@@ -1,14 +1,17 @@
-"use client";
+'use client';
 
 import { useRouter } from 'next/navigation';
+import styles from '../styles/Success.module.css'; 
 
 const Success = () => {
   const router = useRouter();
 
   return (
-    <div>
-      <h1>회원가입이 성공적으로 완료되었습니다!</h1>
-      <button onClick={() => router.push('/login')}>로그인 페이지로 이동</button>
+    <div className={styles.successContainer}>
+      <h1 className={styles.successMessage}>회원가입이 성공적으로 완료되었습니다!</h1>
+      <button onClick={() => router.push('/login')} className={styles.successButton}>
+        로그인 페이지로 이동
+      </button>
     </div>
   );
 };
